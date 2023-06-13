37e2611ac92d9bf8f655bc398dfe99870f79c2c4
Janmajaya Mall <janmajayamall@protonmail.com>
2023-06-13 10:08:32 +0530

    add NTT_Destroy to header

--- hexl/include/hexl/c/ntt.h
+++ hexl/include/hexl/c/ntt.h
@@ -13,3 +13,5 @@ FUNC NTT_ComputeForward(void* thisptr, uint64_t* result,
 FUNC NTT_ComputeInverse(void* thisptr, uint64_t* result,
                         const uint64_t* operand, uint64_t input_mod_factor,
                         uint64_t output_mod_factor);
+
+FUNC NTT_Destroy(void* thisptr);
